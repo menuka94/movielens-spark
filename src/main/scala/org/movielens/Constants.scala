40945dae961db2a6e061458c13d25a8d37b6e415
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-11-03 20:41:25 -0600

    feat: create SparkConf, SparkSession, read data from disk

--- src/main/scala/org/movielens/Constants.scala
+++ src/main/scala/org/movielens/Constants.scala
@@ -3,4 +3,5 @@ package org.movielens
 object Constants {
   final val SPARK_MASTER: String = System.getenv("SPARK_MASTER")
   final val DATA_DIR: String = System.getenv("DATA_DIR")
+  final val USE_HDFS: Boolean = System.getenv("USE_HDFS").toBoolean
 }
--- src/main/scala/org/movielens/Main.scala
+++ src/main/scala/org/movielens/Main.scala
@@ -1,7 +1,28 @@
 package org.movielens
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
 object Main {
   def main(args: Array[String]): Unit = {
     println(s"SPARK_MASTER: ${Constants.SPARK_MASTER}")
+    if (Constants.USE_HDFS) {
+      println("Using HDFS")
+    } else {
+      println("Using local storage")
+      println(s"DATA_DIR: ${Constants.DATA_DIR}")
+    }
+
+    val conf: SparkConf = new SparkConf()
+      .setMaster(Constants.SPARK_MASTER)
+      .setAppName("MovieLens-Spark")
+
+    val spark: SparkSession = SparkSession.builder()
+      .config(conf)
+      .getOrCreate()
+
+    val moviesDF = spark.read.format("csv").load(s"${Constants.DATA_DIR}/movies.csv")
+    println(s"No. of rows: ${moviesDF.count()}")
+    moviesDF.show(10)
   }
 }
